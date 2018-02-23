043f337d168538281ff4dd41e56e59862a2eb481
Breno <takiuti.breno@gmail.com>
2018-02-23 02:58:13 -0300

    23022018a - Fixed function
    Fixed the WM_reflection_bem function to be applicable to any boundary condition, not only reflection

--- WM_reflection_beam.m
+++ WM_reflection_beam.m
@@ -1,4 +1,4 @@
-function [RT,PhiQ_pT,PhiQ_nT,PhiF_pT,PhiF_nT] = WM_reflection_beam(rho,A,E,In,w,AA,BB)
+function [PhiQ_pT,PhiQ_nT,PhiF_pT,PhiF_nT] = WM_reflection_beam(rho,A,E,In,w)
 
 k = abs(sqrt(w)*(rho*A/E/In)^(1/4));
 
@@ -11,7 +11,7 @@ PhiF_pT = E*In*k^2*[1i*k -k; -1 1];
 PhiF_nT = E*In*k^2*[-1i*k k; -1 1];
 size(PhiQ_pT);
 % Use the analytical matrices
-Ct = AA*PhiQ_nT - BB*PhiF_nT;
-Dt = BB*PhiF_pT - AA*PhiQ_pT;
-
-RT = (inv(Ct)*Dt);
+% Ct = AA*PhiQ_nT - BB*PhiF_nT;
+% Dt = BB*PhiF_pT - AA*PhiQ_pT;
+% 
+% RT = (inv(Ct)*Dt);
