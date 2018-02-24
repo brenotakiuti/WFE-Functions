18218542dd6d7191902f0acb48c293122b680308
Breno <takiuti.breno@gmail.com>
2018-02-24 17:29:39 -0300

    24022018a - Corrected WA and WM
    The equations for the analytical beam was wrong and the WM function for the rod was created.

--- WA_reflection_beam_area.m
+++ WA_reflection_beam_area.m
@@ -9,4 +9,4 @@ EE = [-1i*k1 k1; -1 1];
 FF = beta^2*[1i*k2 -k2; -1 1];
 
 R = (EE-FF*CC^-1*BB)^-1*(FF*CC^-1*AA-DD);
-T = (FF-EE*BB^-1*CC)^-1*(-EE*BB^-1*CC+DD);
+T = (FF-EE*BB^-1*CC)^-1*(-EE*BB^-1*AA+DD);
